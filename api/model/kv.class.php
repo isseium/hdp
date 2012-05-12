14260c8649e05956036caccaf5f11a25fd818cd2
KOMATSU Issei <issei@mizuhataya.com>
2012-05-12 17:18:55 +0900

    maybe ok

---
+++ api/model/kv.class.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Zend/Db/Table/Abstract.php';
+
+class Match extends Zend_Db_Table_Abstract
+{
+	protected $_name = 'kv';
+}
+
---
+++ api/public/api.php
@@ -0,0 +1,44 @@
+<?php
+// 環境
+define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+require_once 'Zend/Rest/Server.php';
+require_once 'Zend/Config/Ini.php';
+require_once '../model/kv.class.php';
+require_once 'Zend/Oauth/Token/Access.php';
+require_once 'Zend/Service/Twitter.php';
+require_once 'Zend/Db/Expr.php';
+
+class HappyDinnerAPI
+{
+    public function __construct()
+    {
+        // DB
+        $config = new Zend_Config_Ini('../configs/application.ini', APPLICATION_ENV);
+        $db = Zend_Db::factory($config->database);
+        Zend_Db_Table_Abstract::setDefaultAdapter($db);
+    }
+
+    public function getkvs($key){
+        $kv = new KV();
+        $select = $kv->select()->where('`key` = ?', $key);
+        $row = $kv->fetchrow($select);
+        return array('value' => $row->value);
+    }
+
+    public function setkvs($key, $value){
+        $kv = new KV;
+        try {
+             $kv->insert(array('key' => $key, 'value' => $value));
+        } catch ( Exception $e ){
+             // insert 失敗時は update することで upsert を実現
+             $where = $kv->getAdapter()->quoteInto('`key` = ?', $key);
+             $kv->update(array('value' => $value), $where);
+        }
+
+    }
+}
+
+$server = new Zend_Rest_Server();
+$server->setClass('HappyDinnerAPI');
+$server->handle();
